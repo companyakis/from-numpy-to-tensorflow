f554ff3e8204d59ec3e3caee89dc2f550927fbcb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-13 08:52:04 +0300

    Create part1-0006-model-function1.py

---
+++ part1-0006-model-function1.py
@@ -0,0 +1,19 @@
+"""
+ let's create a Python function
+
+ remember our model
+
+ two inputs -> age, height
+
+ one hidden layer -> two nodes
+
+ one output -> kg
+
+ we should consider min and max weight values
+
+"""
+
+def weight_inits(min_val, max_val, number_of_hidden_layer_nodes, number_of_input_features):
+
+  pass
+
