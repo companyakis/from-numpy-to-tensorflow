453ef28cb6112101cd69e98ec5b9170be9ae74df
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-14 12:21:36 +0300

    Create part1-0014-early-stopping1.py

---
+++ part1-0014-early-stopping1.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+# person data
+
+age = 32
+
+height = 174
+
+real_kg = 85
+
+model_inputs = np.array([age, height])
+
+model_output = np.array([real_kg])
+
+# let's assign initial weights for simplicity
+
+model_weights = np.array([8, -1])
+
+# let's set early stopping value
+
+# we mean if model error exceeds this value, stop the process!
+
+early_stopping = 25
+
+# assume we have 10 epochs
+
+for epoch in range(10):
+  # feed forward
+  predictions = np.dot(model_inputs, model_weights)
+
+  # error
+  error = model_output - predictions
+
+  # slope partial derivative
+  slope = 2 * model_inputs * error
+
+  # model should learn => learning rate
+  # let's assign an initial value
+  learning_rate = 0.0001
+
+  model_weights = model_weights - (learning_rate * slope)
+
+  epoch_l.append(epoch + 1) # range(10) starts from zero
+
+  model_w1_l.append(model_weights[0])
+
+  model_w2_l.append(model_weights[1])
+
+  final_epoch_error =  model_output - np.dot(model_inputs, model_weights)
+
+  error_l.append(final_epoch_error)
+
+  if final_epoch_error > early_stopping:
+    break
