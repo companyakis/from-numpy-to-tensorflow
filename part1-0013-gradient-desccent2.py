7c07ca360b0602e5979b89ee080570998a28dfe2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-14 11:23:05 +0300

    Create part1-0013-gradient-desccent2.py

---
+++ part1-0013-gradient-desccent2.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# person data
+
+age = 32
+
+height = 174
+
+real_kg = 85
+
+model_inputs = np.array([age, height])
+
+model_output = np.array([real_kg])
+
+# let's assign initial weights for simplicity
+
+model_weights = np.array([8, -1])
+
+# assume we have 10 epochs
+
+for epoch in range(10):
+  # feed forward
+  predictions = np.dot(model_inputs, model_weights)
+
+  # error
+  error = model_output - predictions
+
+  # slope partial derivative
+  slope = 2 * model_inputs * error
+
+  # model should learn => learning rate
+  # let's assign an initial value
+  learning_rate = 0.0001
+
+  model_weights = model_weights - (learning_rate * slope)
