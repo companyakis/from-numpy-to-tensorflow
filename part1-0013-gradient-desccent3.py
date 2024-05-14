874799ceef54db975ddb8bbbdc9e60f063c5ceb3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-14 11:35:40 +0300

    Create part1-0013-gradient-desccent3.py

---
+++ part1-0013-gradient-desccent3.py
@@ -0,0 +1,17 @@
+"""
+
+next steps
+
+1- create 4 lists => epoch, model weight 1, model weight 2 and error
+
+2- create model history dataframe
+
+"""
+
+epoch_l = []
+
+model_w1_l = []
+
+model_w2_l = []
+
+error_l = []
