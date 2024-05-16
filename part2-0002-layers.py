c49a42268b62f2209ed7a19f0bcba2039f472539
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-16 20:59:30 +0300

    Create part2-0002-layers.py

---
+++ part2-0002-layers.py
@@ -0,0 +1,17 @@
+# input layer
+
+model.add(tf.keras.layers.Input(shape = (2, 0)))
+
+# hidden layers
+
+# let's add 3 hidden layers
+
+model.add(tf.keras.layers.Dense(40, activation = "relu"))
+
+model.add(tf.keras.layers.Dense(30, activation = "relu"))
+
+model.add(tf.keras.layers.Dense(20, activation = "relu"))
+
+# output layer
+
+model.add(tf.keras.layers.Dense(1))
