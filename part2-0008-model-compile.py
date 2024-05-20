710b96cd3effd5b8e54c3d38d822ff3517c696f4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-20 09:30:14 +0300

    Update part2-0008-model-compile.py

--- part2-0008-model-compile.py
+++ part2-0008-model-compile.py
@@ -1,3 +1,27 @@
+import tensorflow as tf
+
+model = tf.keras.Sequential()
+
+# input layer
+
+model.add(tf.keras.layers.Input(shape = (2, )))
+
+# hidden layers
+
+# let's add 3 hidden layers
+
+model.add(tf.keras.layers.Dense(40, activation = "relu"))
+
+model.add(tf.keras.layers.Dense(30, activation = "relu"))
+
+model.add(tf.keras.layers.Dense(20, activation = "relu"))
+
+# output layer
+
+model.add(tf.keras.layers.Dense(1, activation = "relu"))
+
+# let's compile
+
 model.compile(
     optimizer = "sgd",
     loss = "mean_squared_error",
