32c7fddd2e08cfae02312f75a814b01b56364a8b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-30 14:13:56 +0300

    Update part4-0014-prediction.py

--- part4-0014-prediction.py
+++ part4-0014-prediction.py
@@ -1,16 +1,17 @@
-print(predictions[100])
+print(predictions[650])
 
 # remember
 
 # img_labels = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
 
-# look at the result -> max value is 8.9770186e-01 -> index is 5 -> is it a deer?
+# look at the result -> max value is 0.961503 -> index is 3 -> is it a cat?
 
 # let's visualize
 
-plt.imshow(test_images[100])
+plt.imshow(test_images[650])
 
 """
-[3.2967605e-02 4.6841739e-04 6.2942159e-01 7.9740661e-01 8.9414394e-01
- 8.9770186e-01 4.9509215e-01 8.8792223e-01 2.5936970e-02 2.0977426e-02]
+[0.06860047 0.00142178 0.90365267 0.961503   0.51962835 0.6906132
+ 0.6789461  0.2150917  0.03914241 0.01890532]
+
 """
