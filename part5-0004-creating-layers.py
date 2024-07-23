f2f16d5eca54dedccae4695ba0825ee12560454b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-23 11:13:32 +0300

    Update part5-0004-creating-layers.py

--- part5-0004-creating-layers.py
+++ part5-0004-creating-layers.py
@@ -6,9 +6,9 @@ normalization_layer = tf.keras.layers.Normalization()
 
 hidden_layer_1 = tf.keras.layers.Dense(30, activation = "relu")
 
-hidden_layer_2 = tf.keras.layers.Dense(20, activation = "relu")
+hidden_layer_2 = tf.keras.layers.Dense(30, activation = "relu")
 
-hidden_layer_3 = tf.keras.layers.Dense(10, activation = "relu")
+hidden_layer_3 = tf.keras.layers.Dense(30, activation = "relu")
 
 concatenation_layer = tf.keras.layers.Concatenate()
 
