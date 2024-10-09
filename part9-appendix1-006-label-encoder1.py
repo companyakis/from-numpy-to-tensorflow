dc2056c9ce51a5d37444b1fb45179dec6e7f053b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-09 14:17:07 +0300

    Create part9-appendix1-006-label-encoder1.py

---
+++ part9-appendix1-006-label-encoder1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+iris = pd.read_csv("./iris/iris.data", names = ["sepal length in cm", "sepal width in cm", "petal length in cm", "petal width in cm", "class"])
+
+print(iris["class"].value_counts())
+
+"""
+class
+Iris-setosa        50
+Iris-versicolor    50
+Iris-virginica     50
+Name: count, dtype: int64
+
+"""
+
+x = iris.iloc[:, :4]
+
+y = iris.iloc[:, -1]
+
