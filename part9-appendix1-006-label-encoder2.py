e7065b10f8e7c39117291744927ca6d0f8589a8c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-09 14:21:25 +0300

    Create part9-appendix1-006-label-encoder2.py

---
+++ part9-appendix1-006-label-encoder2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+iris = pd.read_csv("./iris/iris.data", names = ["sepal length in cm", "sepal width in cm", "petal length in cm", "petal width in cm", "class"])
+
+encoder = LabelEncoder()
+
+x = iris.iloc[:, :4]
+
+y = iris.iloc[:, -1]
+
+y = encoder.fit_transform(y)
+
+print(y)
+
+"""
+[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+ 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2
+ 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+ 2 2]
+"""
