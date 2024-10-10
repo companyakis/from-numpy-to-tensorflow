3f2ef5e703013e4ef828995709a420eaad2f7b5e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-10 23:12:37 +0300

    Create part9-appendix1-007-local-outlier-factor1.py

---
+++ part9-appendix1-007-local-outlier-factor1.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+data = pd.read_csv("diabetes.csv")
+
+print(data.head(3))
+
+print("--------------------------------------")
+
+print(data.describe())
+
+print("--------------------------------------")
+
+print(data.info())
+
+"""
+   Pregnancies  Glucose  BloodPressure  SkinThickness  Insulin   BMI  DiabetesPedigreeFunction  Age  Outcome
+0            6      148             72             35        0  33.6                     0.627   50        1
+1            1       85             66             29        0  26.6                     0.351   31        0
+2            8      183             64              0        0  23.3                     0.672   32        1
+--------------------------------------
+       Pregnancies     Glucose  BloodPressure  SkinThickness     Insulin         BMI  DiabetesPedigreeFunction         Age     Outcome
+count   768.000000  768.000000     768.000000     768.000000  768.000000  768.000000                768.000000  768.000000  768.000000
+mean      3.845052  120.894531      69.105469      20.536458   79.799479   31.992578                  0.471876   33.240885    0.348958
+std       3.369578   31.972618      19.355807      15.952218  115.244002    7.884160                  0.331329   11.760232    0.476951
+min       0.000000    0.000000       0.000000       0.000000    0.000000    0.000000                  0.078000   21.000000    0.000000
+25%       1.000000   99.000000      62.000000       0.000000    0.000000   27.300000                  0.243750   24.000000    0.000000
+50%       3.000000  117.000000      72.000000      23.000000   30.500000   32.000000                  0.372500   29.000000    0.000000
+75%       6.000000  140.250000      80.000000      32.000000  127.250000   36.600000                  0.626250   41.000000    1.000000
+max      17.000000  199.000000     122.000000      99.000000  846.000000   67.100000                  2.420000   81.000000    1.000000
+--------------------------------------
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 768 entries, 0 to 767
+Data columns (total 9 columns):
+ #   Column                    Non-Null Count  Dtype
+---  ------                    --------------  -----
+ 0   Pregnancies               768 non-null    int64
+ 1   Glucose                   768 non-null    int64
+ 2   BloodPressure             768 non-null    int64
+ 3   SkinThickness             768 non-null    int64
+ 4   Insulin                   768 non-null    int64
+ 5   BMI                       768 non-null    float64
+ 6   DiabetesPedigreeFunction  768 non-null    float64
+ 7   Age                       768 non-null    int64
+ 8   Outcome                   768 non-null    int64
+dtypes: float64(2), int64(7)
+
+"""
